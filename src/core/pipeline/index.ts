dd28d3a43fcc536e9c0957df56fa20c15f5aee94
Pauline <mpgxvii@gmail.com>
2026-09-06 08:10:28 +0800

    feat: Add new DataPipeline to interface between data producers and KafkaService

---
+++ src/core/pipeline/index.ts
@@ -0,0 +1,8 @@
+export { SchemaType } from './types';
+export type { KeyExport, SchemaMetadata, Converter } from './types';
+export { ConverterFactory } from './converters';
+export {
+  DefaultDataPipeline,
+  dataPipelineFactory,
+} from './DataPipeline';
+export type { DataPipelineService } from './DataPipeline';
